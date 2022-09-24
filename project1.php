6777d8ee15e45002cc724e644e6793d7b7869a16
owenknight2210 <114300060+owenknight2210@users.noreply.github.com>
2022-09-24 21:55:11 +0700

    Update project1.php

--- project1.php
+++ project1.php
@@ -1,1 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
+<style>
+.luongLigma{
+    width: 1100px;
+    margin: 0 auto;
+}
+.container {
+   width: 1100px;
+   margin: 0 auto;
+   border: 1px solid #333;
+   padding: 10px;
+   text-align: center;
+   display: inline-block;
+   background-color: transparent;
+   background-image: url('https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=');
+}
+#post {
+   width: 300px;
+   height: 60px;
+   float: left;
+}
+#sidebar {
+ width: 660px;
+ height: 60px;
+ background: #fff;
+ float: right;
+ background-color: transparent;
+}
+.clear { clear: both }
+.ligMa_1{
+    padding-top: 13px;
+}
+.ligMa_1  img{
+    width: 110px;
+    height: 40px;
+    float: left;
+}
+.ligMa_2{
+    height: 60px;
+    float: left;
+    padding-top: 20px;
+}
+.ligMa_2 select{
+    width: 120px;
+    height: 40px;
+    font-size: 15px;
+    
+    font-family: 'Raleway', Arial, sans-serif;
+    border: none;
+    background-color: #5666a5;
+    color: #ffffff;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 46px;
+    outline: none;
+    position: relative;
+    overflow: hidden;
+    font-size: 14px;
+    border-radius: 23px;
+    letter-spacing: 2px;
+    -webkit-transition: all 0.35s ease;
+    transition: all 0.35s ease;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.ligMa_3{
+    float: left;
+    padding-top: 20px;
+    padding-left: 20px;
+}
+.ligMa_3 button{
+    width: 120px;
+    height: 40px;
+
+    font-family: 'Raleway', Arial, sans-serif;
+    border: none;
+    background-color: #5666a5;
+    border-radius: 5px;
+    color: #ffffff;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 46px;
+    font-weight: 400;
+    font-size: 1em;
+    outline: none;
+    position: relative;
+    overflow: hidden;
+    font-size: 14px;
+    border-radius: 23px;
+    letter-spacing: 2px;
+    -webkit-transition: all 0.35s ease;
+    transition: all 0.35s ease;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+
+    }
+.ligMa_4{
+    height: 60px;
+    float: left;
+    padding-top: 20px;
+    padding-left: 30px;
+
+}
+.ligMa_4 select{
+    width: 50px;
+    height: 40px;
+    font-size: 30px;
+
+    font-family: 'Raleway', Arial, sans-serif;
+    border: none;
+    background-color: #5666a5;
+    border-radius: 5px;
+    color: #ffffff;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 46px;
+    font-weight: 400;
+    font-size: 1em;
+    outline: none;
+    position: relative;
+    overflow: hidden;
+    font-size: 14px;
+    border-radius: 23px;
+    letter-spacing: 2px;
+    -webkit-transition: all 0.35s ease;
+    transition: all 0.35s ease;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;    
+}
+.ligMa_5{
+    height: 60px;
+    float: right;
+    padding-top: 20px;
+    
+}
+.snip1582 {
+    background-color: #c47135;
+    border: none;
+    color: #ffffff;
+    display: inline-block;
+    font-size: 20px;
+    text-transform: uppercase;
+    font-weight: 500;
+    height: 45px ;
+}
+</style>
+<body>
+<div class="luongLigma">
+
+    <div id="content" class="container">
+        <div id="post">
+            <div class="ligMa_1">
+                <a href="#"><img src="https://cdn-images-1.medium.com/max/800/1*hCLxRgOtiWBy4ykQZ1toWQ@2x.png"></a>
+            </div>
+        </div>
+
+        <div id="sidebar">
+            <div class="ligMa_2">
+            <select>
+                <option value="KHAMPHA">Khám phá</option>
+                <option value="NOIDUNG">Nội dung tìm kiếm phổ biến</option>
+                <option value="BANGXEPHANG">Bảng xếp hạng</option>
+            </select>
+            </div>
+            
+            <div class="ligMa_3">
+                <a href="https://knowyourmeme.com/memes/ligma"><button>Giấy phép</button></a>
+            </div>
+            <div class="ligMa_3">
+                <a href="https://knowyourmeme.com/memes/oh-my-god-ohmagaa-sound-effect"><button>Tải lên</button></a>
+            </div>
+            <div class="ligMa_4">
+                <select >
+                    <option value="chamcham">...</option>
+                    <option style="font-size: 15px;" value="thamgia">Tham gia</option>
+                    <option style="font-size: 15px;" value="tdnn">Thay đổi ngôn ngữ</option>
+                </select>
+            </div>
+            <div class="ligMa_5">
+                <button class="snip1582">Tham gia</button>
+            </div>
+        </div> 
+        <div class="clear"></div>
+    </div>
+
+    <div class="ligMaBall">
+        <img src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/307755020_3351254958478756_869656501297043234_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=_yHoeYkZW0EAX8fQE2X&_nc_ht=scontent.fhan17-1.fna&oh=00_AT9brE7ZxzlBrrt_3LKRqkx8qx3RgprlTMjCCSNtUftdEQ&oe=632DF943">
+    </div>
+    <div>
+        <a href="#"><img src=""></a>
+    </div>
+</div>
+</body>
+</html>
